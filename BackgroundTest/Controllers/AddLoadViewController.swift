9f4b360eb33fd1cbc4166f197256af8aa52cee5e
Dean Woodward <dean.woodward@datacom.co.nz>
2016-04-17 11:26:08 +1200

    Initial project setup.

---
+++ BackgroundTest/Controllers/AddLoadViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddLoadViewController.swift
+//  BackgroundTest
+//
+//  Created by Dean Woodward on 17/04/16.
+//  Copyright © 2016 Dean Woodward. All rights reserved.
+//
+
+import UIKit
+
+class AddLoadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func submitLoadButtonTapped(sender: UIButton) {
+        
+    }
+    
+}
